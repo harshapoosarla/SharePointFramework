e8ae058161420919875e762defd0cef08598084e
harshapoosarla <43164668+harshapoosarla@users.noreply.github.com>
2018-10-26 12:32:09 +0530

    bootstrap update

--- helloworld-webpart/src/webparts/helloworld/HelloworldWebPart.ts
+++ helloworld-webpart/src/webparts/helloworld/HelloworldWebPart.ts
@@ -6,6 +6,7 @@ import {
   PropertyPaneDropdown
 } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { SPComponentLoader } from '@microsoft/sp-loader'; 
 
 import styles from './HelloworldWebPart.module.scss';
 import * as strings from 'HelloworldWebPartStrings';
@@ -13,84 +14,70 @@ import {IHelloworldWebPartProps} from './HelloworldWebPartProps'
 import { SPHttpClientResponse, SPHttpClient } from '@microsoft/sp-http';
 import { SPListItem } from '@microsoft/sp-page-context';
 import * as $ from "jquery"; 
+//import "jquery";
+require("bootstrap");
 
 
 export default class HelloworldWebPart extends BaseClientSideWebPart<IHelloworldWebPartProps> {
 
   public render(): void {
+    let cssURL = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+    SPComponentLoader.loadCss(cssURL); 
     this.domElement.innerHTML = `
       <div class="${ styles.helloworld }">
         <div class="${ styles.container }">
           <div class="${ styles.row }" style="background-color:${escape(this.properties.color)};">
             <div class="${ styles.column }">
-
-            <p> Name:&nbsp&nbsp   <input type="text" name="name"><br></p>
-            <p> Gender:
-            <select>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            </select><br>
-            </p>
-            <p>
-            <button id="savedata" type="button" style="margin:auto;display:block">Submit</button>
-            </p>
-
+              <div class="form-group">
+                <label for="usr">Name:</label>
+                <input type="text" class="form-control" id="usr">
+                
+                <label for="usr">Gender:</label></br>
+                <select class="form-control" id="genderDrop">
+                <option >Male</option>
+                <option >Female</option>
+                </select> </br>
+               
+                <button type="button" class="btn btn-primary btn-sm">Submit</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-     
-      <div id="lists"></div>
-      <div id="listitems" style="background-color:${escape(this.properties.color)};">
-      Category:<select id="items">
-      </select>
+    </div>
+  <div id="lists"></div>
       
+      <div id="listitems" style="background-color:${escape(this.properties.color)};">
+           Category:<select id="items">
+           </select>
       </div>
+      
       <div id="products" style="background-color:${escape(this.properties.color)};"></div>`;
       this.domElement.querySelector('#items').addEventListener('change',()=>this.getProducts());
       this.getListsInfo();
       this.getListItems();
-      this.eventhandlersss();
+     // this.eventhandlersss();
       this.getProducts();
-      //this.changed();
-      //this.saveData();
    }
    
-   eventhandlersss(){
-     alert("asddas");
+  //  eventhandlersss(){
      
-   //  document.getElementById('items').addEventListener('change',()=>this.getProducts());
-   }
-  //  public changed():void{
-  //    $(document).ready(function(){
-  //     alert("ready");
-  //     $('#items').change(function(){
-  //       alert("come");
-  //       var selectedval=$(this);
-  //       alert(selectedval);
-  //     });
-  //    });
+  //   }
+  //<span class="${ styles.title }">Welcome to SharePoint!</span>
+  //<p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
+  //<p class="${ styles.description }">${escape(this.properties.description)}</p>
+  //<p class="${ styles.description }">${escape(this.properties.color)}</p>
+  //<a href="https://aka.ms/spfx" class="${ styles.button }">
+  //<span class="${ styles.label }">Learn more</span>
+  //</a>
+  //style="background-color:${escape(this.properties.color)};
 
-  //  }
-   //<span class="${ styles.title }">Welcome to SharePoint!</span>
-   //<p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-   //<p class="${ styles.description }">${escape(this.properties.description)}</p>
-   //<p class="${ styles.description }">${escape(this.properties.color)}</p>
-   //<a href="https://aka.ms/spfx" class="${ styles.button }">
-   //<span class="${ styles.label }">Learn more</span>
-   //</a>
-//style="background-color:${escape(this.properties.color)};
-  //public saveData(){
-    //   alert("data saved to list");
-
-  //}
   public getListsInfo() {
     let html: string = '';
     if (Environment.type === EnvironmentType.Local) {
       this.domElement.querySelector('#lists').innerHTML = "Sorry this does not work in local workbench";
     } else {
-    this.context.spHttpClient.get
-    (
+    this.context.spHttpClient.get(
       this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=Hidden eq false`, 
       SPHttpClient.configurations.v1)
       .then((response: SPHttpClientResponse) => {
@@ -128,27 +115,7 @@ export default class HelloworldWebPart extends BaseClientSideWebPart<IHelloworld
     }
   }
  public getProducts() {
-  //if(Environment.type === EnvironmentType.Local){
-    // this.domElement.querySelector('#productresult').innerHTML = `error occured in the web part`;
-    // }else{
-    // this.context.spHttpClient.get(
-    // this.context.pageContext.web.absoluteUrl+"/_api/Web/Lists/getByTitle('Products')/items?$select=Title,ID&$filter=(Category/ID eq "+selectedValue+")", SPHttpClient.configurations.v1
-    // ).then((Respons: SPHttpClientResponse)=>{
-    // Respons.json().then((item : any)=>{
-    // item.value.forEach(SPListItem =>{
-    // categoryData += `
-    // <ul>
-    // <li>
-    // <span class="ms-font-l">${SPListItem.Title}</span>
-    // </li>
-    // </ul>`;
-    // });
-    // this.domElement.querySelector('#productresult').innerHTML = categoryData;
-    // });
-    
-    // });
-    // } 
-    alert("products displayed");
+  alert("products displayed");
     var product= $('#items').val();
     alert("product"+product);
     let html: string = '';
